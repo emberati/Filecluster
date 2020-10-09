ec069afaba46aebecb3da85fa2bd6a7f2658ca7d
emberati <alex.shak55@gmail.com>
2020-10-09 18:38:17 +0300

    Pre-release test

--- fileclaster.py
+++ fileclaster.py
@@ -3,9 +3,6 @@ from sys import argv
 
 path = argv[len(argv) - 1]
 
-def main():
-    global path
-
 #    Присваивает глобальной переменной path значение пути,
 #    в котором _расположен_запускаемый_файл_скрипта_.
 #
@@ -14,15 +11,6 @@ def main():
 
     # -> path = str(os.path.dirname(__file__)) + "\\" + str(path)
 
-    size_ = os.path.getsize(path)
-
-    mib = get_mib(size_)
-    kib = get_kib(size_)
-
-    print("File size is " + str(round(mib, 4)) + "MB.");
-    print("File size is " + str(round(kib, 4)) + "KB.");
-    print("File size is " + str(size_) + "B.");
-
 
 def get_mib(size_in_bytes):
     return size_in_bytes / pow(1024, 2)
@@ -30,54 +18,70 @@ def get_mib(size_in_bytes):
 def get_kib(size_in_bytes):
     return size_in_bytes / 1024
 
-
 #=============================================================================#
 #                                 Обход папок                                 #
 #=============================================================================#
 
 #       Возвращает отформатированный список файлов и папок директории
-def str_content(path):
-    content = os.path.abspath(path) + '\n'
-    isdir = ''
-    index = 0
-    for i in os.listdir(path):
-        if os.path.isdir(path + '\\' + i):
-            isdir = '<DIR>'
-        else:
-            isdir = str(os.path.getsize(path + '\\' + i)) + 'B'
-        content += str(index) + '.  ' + isdir  + '\t' + i + '\n'
-        index += 1
-    return content
+
+#def str_content(path):
+#    content = os.path.abspath(path) + '\n'
+#    isdir = ''
+#    index = 0
+#    for i in os.listdir(path):
+#        if os.path.isdir(path + '\\' + i):
+#            isdir = '<DIR>'
+#        else:
+#            isdir = str(os.path.getsize(path + '\\' + i)) + 'B'
+#        content += str(index) + '.  ' + isdir  + '\t' + i + '\n'
+#        index += 1
+#    return content
 
 #       Обходит дерево каталогов, начиная с корневого
 filecount = 0
+skipped = 0
+sysdir = []
 def bypass(path, size = 0):
     global filecount
-    try:
-        print(str_content(path))
-        None
-    except PermissionError: None
+    global skipped
+
+#    try:
+#        print(str_content(path))
+#        None
+#    except PermissionError: None
 
     current = 0
     for file in os.listdir(path):
+        node = path + '\\' + file
         try:
-            if os.path.isdir(path + '\\' + file):
+            if os.path.isdir(node):
                 print('\tDown to: ', file)
-                size = bypass(path + '\\' + file, size)
+                size = bypass(node, size)
                 print('\tUp to: ', path)
             else:
-                current = os.path.getsize(path + '\\' + file)
+                current = os.path.getsize(node)
                 filecount += 1
                 size += current
-            print(size)
-        except PermissionError: continue
+        except PermissionError:
+            sysdir.append(node)
+            skipped += 1
+            continue
     return size
 
 def sbytes(bytes):
     return bytes / filecount
+
 #           Зона глобальной видимости
 bytes = bypass(path)
 
-print('\n')
-print(get_kib(sbytes(bytes)))
-#print((1746 + 9307472 + 12242 + 652601 + 282))
+print()
+print('Analyzed data size:', str(bytes) + 'B')
+print('Files:', filecount, '\n')
+print('Skipped system directories:', skipped)
+
+for i in sysdir:
+    print(i)
+
+print()
+bytes = sbytes(bytes)
+print('Average file size', str(round(get_kib(bytes), 2)) + 'KB')
